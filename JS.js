9df024f751fb6e58d09233539fa7a02580b92034
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-03-23 18:57:39 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -13,4 +13,14 @@ var a = 9;
 var myFirstName = "Maxim";
 var myLastName = "Mikhaylov";
 
-
+// Only change code below this line
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+c = "I am a"
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
