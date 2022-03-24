470e73f4439160318278810fed30df05fc414c8b
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-03-24 12:29:26 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -72,10 +72,27 @@ const remainder = 11 % 3;
 let a = 3;
 let b = 17;
 let c = 12;
-
 // Only change code below this line
 a += 12;
 b += 9;
 c += 7; */
+
+/* 
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1; */
+
+/*
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= b;
+c *= 10; */
 
 
