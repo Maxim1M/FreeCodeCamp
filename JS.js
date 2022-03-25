8b4759a2f857b58a84cbd1d715d0c2dd18dc21e7
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-03-25 14:19:21 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -99,4 +99,5 @@ c *= 10; */
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>'; */
 
-const myStr = "FirstLine\n\tSecondLine\nThirdLine";
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
