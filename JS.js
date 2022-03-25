e1ce8264b7ac2368a7fbcfaabd97f1ddc3fcc6a2
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-03-25 16:01:06 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -130,3 +130,8 @@ firstLetterOfLastName = lastName[0]; // Change this line
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+// Setup
+//const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
