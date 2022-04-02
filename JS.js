208547171cc7438602aac89612a40b45c985dfeb
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-04-02 14:10:59 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -162,12 +162,12 @@ var wordBlanks = "My "+ myAdjective +" and dumb " + myNoun +" has "+ myVerb +" v
 
 //const myArray = [["Bulls",23],["White Sox",45]];
 
-const myArray = [50, 60, 70];
+//const myArray = [50, 60, 70];
 myArray[0]; 
 const myData = myArray[0];
 
 // Setup
-const myArray = [18, 64, 99];
+//const myArray = [18, 64, 99];
 myArray[0] = 45;
 // Only change code below this line
 
