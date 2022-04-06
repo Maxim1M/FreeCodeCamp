ecb973d3b5f1f5692c4be0fe9ce74cf6620ae8a1
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-04-06 22:14:29 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -209,9 +209,9 @@ function reusableFunction() {
 }
 reusableFunction();
 
-function functionWithArgs(){
-  console.log(1+2);
-  console.log(7+9);
+function functionWithArgs(a, b) {
+  console.log(a + b)
 }
+
 functionWithArgs(1,2);
 functionWithArgs(7,9);
