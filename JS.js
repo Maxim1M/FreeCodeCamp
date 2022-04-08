7761d933ed975b453856c310b66f53b0fb6b576c
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-04-08 21:21:46 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -244,3 +244,19 @@ function minusSeven(num) {
   console.log(timesFive(0));
   
   console.log(minusSeven(10));
+
+// Declare the myGlobal variable below this line
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+}
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
